39b570d7062ab1657693a592829cc6da8e7a5400
Anna Ivanchenko <bubachka1990@gmail.com>
2026-06-09 00:31:33 +0300

    fix

--- app/notes/filter/[...slug]/Notes.client.tsx
+++ app/notes/filter/[...slug]/Notes.client.tsx
@@ -1,36 +1,24 @@
 'use client';
-import 'modern-normalize/modern-normalize.css';
 
-import { useState } from 'react';
 import { useQuery, keepPreviousData } from '@tanstack/react-query';
-import { useDebouncedCallback } from 'use-debounce';
-import { Toaster } from 'react-hot-toast';
-
 import css from './Notes.module.css';
-
 import Pagination from '../../../../components/Pagination/Pagination';
 import SearchBox from '../../../../components/SearchBox/SearchBox';
-import Modal from '../../../../components/Modal/Modal';
-import NoteForm from '../../../../components/NoteForm/NoteForm';
-import NoteList from '../../../../components/NoteList/NoteList';
-
+import { useState } from 'react';
 import { fetchNotes } from '../../../../lib/api';
+import NoteList from '../../../../components/NoteList/NoteList';
+import { useDebouncedCallback } from 'use-debounce';
+import { Toaster } from 'react-hot-toast';
 import { NoteTag } from '@/types/note';
+import Link from 'next/link';
 
 type Props = {
   tag?: NoteTag;
 };
-
-function App({ tag }: Props) {
-  const [createNoteThis, setCreateNoteThis] = useState(false);
+function Notes({ tag }: Props) {
   const [input, setInput] = useState('');
   const [querySe, setQuery] = useState('');
   const [page, setPage] = useState(1);
-
-  const debouncedSetQuery = useDebouncedCallback((value: string) => {
-    setQuery(value);
-    setPage(1);
-  }, 500);
 
   const { data, isLoading, isSuccess, isFetching } = useQuery({
     queryKey: ['notes', { page, querySe, tag }],
@@ -39,18 +27,14 @@ function App({ tag }: Props) {
         page,
         search: querySe || undefined,
         perPage: 12,
-        tag,
+        tag: tag,
       }),
     placeholderData: keepPreviousData,
   });
 
-  const openModal = () => {
-    setCreateNoteThis(true);
-  };
-
-  const closeModal = () => {
-    setCreateNoteThis(false);
-  };
+  const debouncedSetQuery = useDebouncedCallback((value: string) => {
+    setQuery(value);
+  }, 500);
 
   const totalPages = data?.totalPages ?? 0;
   const notes = data?.notes ?? [];
@@ -62,55 +46,35 @@ function App({ tag }: Props) {
       <header className={css.toolbar}>
         <SearchBox
           value={input}
-          onChange={(val) => {
+          onChange={val => {
             setInput(val);
             debouncedSetQuery(val);
+            setPage(1);
           }}
         />
 
         {isSuccess && totalPages > 1 && (
-          <Pagination
-            totalPages={totalPages}
-            page={page}
-            setPage={setPage}
-          />
+          <Pagination totalPages={totalPages} page={page} setPage={setPage} />
         )}
-
-        <button
-          className={css.button}
-          onClick={openModal}
-        >
+        <Link className={css.button} href="/notes/action/create">
           Create note +
-        </button>
+        </Link>
       </header>
 
       {isEmpty ? (
         <p>
-          {tag || querySe
-            ? 'No notes found for this filter'
-            : 'No notes yet'}
+          {tag || querySe ? 'No notes found for this filter' : 'No notes yet'}
         </p>
       ) : (
         !isLoading &&
         !isFetching &&
         isSuccess &&
-        notes.length > 0 && (
-          <NoteList notes={notes} />
-        )
+        data.notes.length > 0 && <NoteList notes={data.notes} />
       )}
 
-      <Toaster
-        position="top-center"
-        reverseOrder={false}
-      />
-
-      {createNoteThis && (
-        <Modal onClose={closeModal}>
-          <NoteForm onClose={closeModal} />
-        </Modal>
-      )}
+      <Toaster position="top-center" reverseOrder={false} />
     </div>
   );
 }
 
-export default App;
+export default Notes;
--- app/notes/filter/[...slug]/page.tsx
+++ app/notes/filter/[...slug]/page.tsx
@@ -1,6 +1,7 @@
 import css from '../[...slug]/Notes.module.css';
 import { fetchNotes } from '../../../../lib/api';
 import NotesClient from './Notes.client';
+import type { Metadata } from 'next';
 import {
   HydrationBoundary,
   QueryClient,
@@ -10,6 +11,37 @@ import { NoteTag } from '@/types/note';
 type Props = {
   params: { slug: string[] };
 };
+type GenerateMetadataProps = {
+  params: Promise<{
+    slug: string[];
+  }>;
+};
+
+export async function generateMetadata({
+  params,
+}: GenerateMetadataProps): Promise<Metadata> {
+  const { slug } = await params;
+  return {
+    title: `Note: ${slug.join('/')}`,
+    description: `Note by: ${slug.join('/')}`,
+    openGraph: {
+      title: `Note: ${slug.join('/')}`,
+      description: `Note by: ${slug.join('/')}`,
+      url: `/notes/filter/${slug.join('/')}`,
+      siteName: 'NoteHub',
+      images: [
+        {
+          url: 'https://ac.goit.global/fullstack/react/notehub-og-meta.jpg',
+          width: 1200,
+          height: 630,
+          alt: 'NoteHub',
+        },
+      ],
+      type: 'article',
+    },
+  };
+}
+
 export default async function NotesPage({ params }: Props) {
   const queryClient = new QueryClient();
 
